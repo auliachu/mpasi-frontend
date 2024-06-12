dae4ba0fb739e1c1d7c749cdef9a851780ca8bcc
Ubuntu <jellywops@myeasympasi.neo.internal>
2024-06-12 13:13:06 +0700

    add env

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000";
+    const url = "http://103.150.191.9:4000";
     const [token,setToken] = useState('');
     const [food_list, setFoodList] = useState([]);
 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '103.150.191.9',
+    port: "5001"
+  },
   plugins: [react()],
 })
